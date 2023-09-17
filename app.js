21745a6891e5a1299c442cc0d14d3d250aea0ebf
BenardOtokpa <benard.otokpa@yahoo.com>
2023-09-17 12:21:42 +0100

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const errorHandler = require('./handler/errorHandler')
 const userRoutes = require('./modules/users/users.routes')
 const transactionRoutes = require('./modules/transactions/transactions.routes')
-
+const cors = require("cors")
 const app = express()
 
 require('dotenv').config()
@@ -19,14 +19,19 @@ mongoose
   })
 
 //Model initialization
-require('./models/users.model');
+require('./models/users.model')
 require('./models/tranactions.model')
 
 app.use(express.json())
+app.use(cors())
 
 //Routes..
 app.use('/api/users', userRoutes)
 app.use('/api/transactions', transactionRoutes)
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({ status: 'Failed', message: 'Not Found!' })
+})
 
 // End of all routes...
 app.use(errorHandler)
